b15c28c8d9783b6fa10a4b6b4d08245b9c330862
WasBeJin <145558881+WasBeJin@users.noreply.github.com>
2024-02-21 14:49:32 +0300

    Add files via upload

---
+++ Labs/lab5/resourses/lab5-1-1.asm
@@ -0,0 +1,25 @@
+
+SECTION .data ;  
+DB 'Введите строку:',10 ;
+ EQU $-msg  
+SECTION .bss   
+SECTION .text 
+GLOBAL _start  
+mov eax,4 
+mov ebx,1 
+mov ecx,msg  
+mov edx,msgLen  
+int 80h  
+mov eax, 3
+mov ebx, 0  
+mov ecx, buf1  
+mov edx, 80 
+int 80h  
+mov eax,4 
+mov ebx,1 
+mov ecx,buf1
+mov edx,buf1
+int 80h
+mov eax,1
+mov ebx,0
+int 80h 
